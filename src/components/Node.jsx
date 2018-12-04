254b56ef7605d08ea1f8fe85cbde8d4a7be804c8
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-12-04 15:38:12 +1100

    Add colors for different stages of the journey

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -4,19 +4,26 @@ import './Node.css';
 class Node extends Component {
 
     stateClass() {
+
+        console.log(this.props.stage + " " + this.props.current);
         let styleClass = 'node';
         if (this.props.intermediate.length > 0) 
             styleClass += ' ' + this.props.intermediate;
+        
         if (this.props.done === true) 
             styleClass += ' done';
+        else if (this.props.current === true) 
+            styleClass += ' current';
+        
         return styleClass;
     }
 
     render() { 
         return (
             <div className={this.stateClass()}>
-                <a href="#">{this.props.children}</a>
+                <a href="#">{this.props.stage}</a>
             </div>
+
         );
     }
 }
--- src/components/ProgressGraph.jsx
+++ src/components/ProgressGraph.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import Node from './Node';
 import './ProgressGraph.css';
 
+// Color the node that is the current node
+// Color nodes that have been completed
+// 
+
 class ProgressGraph extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-            stages: [1,2,4,5,6,7]
+            stages: ["Name","Address","Phone number","University", "Sections", "De","Something"],
+            currentStage: 3
         }
     }
 
@@ -15,16 +20,31 @@ class ProgressGraph extends Component {
         return "";
     }
 
+    handleStageChange() {
+        this.setState({currentStage: this.state.currentStage + 1});
+    }
+
+    isCurrent(stage) {
+        if (stage === this.state.currentStage) return true;
+        return false;
+    } 
+
+    isDone(stage) {
+        if (stage < this.state.currentStage) return true;
+        return false;
+    }
+
     render() { 
         return (  
             <div className="progress-graph">
-                {this.state.stages.map((currStage, index) => 
+                {this.state.stages.map((stage, index) => 
                     (<Node 
                         key={index}
                         stage={index+1} 
-                        done={false}
+                        done={this.isDone(index + 1)}
+                        current={this.isCurrent(index + 1)}
                         intermediate={this.addIntermediaryEdge(index)}>
-                        {currStage}
+                        {stage}
                     </Node>
                     )
                 )}
